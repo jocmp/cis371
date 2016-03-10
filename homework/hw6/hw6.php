e58e01e93f16045fb1b6ffb81d37e8acb48c9af6
Josiah Campbell <campbjos@mail.gvsu.edu>
2016-03-10 12:54:54 -0500

    Add sans-serif font to explainer, update script
    * Wrap script in function and call

--- homework/hw6/hw6.php
+++ homework/hw6/hw6.php
@@ -1,5 +1,6 @@
 <?php
 
+function create_dom() {
     header('Content-Type: text/xml');
     $dom = new DOMDocument('1.0', 'utf-8');
     $dom->preserveWhiteSpace = false;
@@ -8,22 +9,27 @@
     $dom->appendChild($root);
     $companies = get_companies();
     foreach ($companies as $company) {
-        // Create elements
+        // Create company element
         $company_root = $dom->createElement('company');
         $root->appendChild($company_root); 
         foreach ($company as $elem => $value) {
             $company_elem = $dom->createElement($elem);
-            // Append children
+            // Append child
             $company_root->appendChild($company_elem);
             // Append child node information 
             $company_elem->appendChild($dom->createTextNode($value));
         }
     }
     echo $dom->saveHTML();
-    
-    function get_companies() {
-        $response = file_get_contents('http://www.cis.gvsu.edu/~scrippsj/cs371/hw/fortune500.json');
-        $fortune500 = json_decode($response, true);
-        return $fortune500['result'];
-    }
+
+}
+
+function get_companies() {
+    $response = file_get_contents('http://www.cis.gvsu.edu/~scrippsj/cs371/hw/fortune500.json');
+    $fortune500 = json_decode($response, true);
+    return $fortune500['result'];
+}
+
+create_dom();
+
 ?>
