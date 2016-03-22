1240012ea390fec4b6b9279e2ee9b25e9b76f655
Josiah Campbell <campbjos@mail.gvsu.edu>
2016-03-22 19:35:27 -0400

    Update homework 7 with ajax call

---
+++ examples/week8/week7c.php
@@ -0,0 +1,6 @@
+<?php
+	if($_GET['param']==1)
+    echo 'data for the first parameter';
+	else
+    echo 'data for the second parameter';
+?>
---
+++ examples/week8/week7d.php
@@ -0,0 +1,6 @@
+<?php
+	if($_POST['param']==1)
+    echo 'data for the first parameter';
+	else
+    echo 'data for the second parameter';
+?>
