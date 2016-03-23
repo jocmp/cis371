6d7c0c7c0fe7f196833ab077a35f2579c8143fd9
Josiah Campbell <campbjos@mail.gvsu.edu>
2016-03-23 16:13:11 -0400

    Update homework 7

--- homework/hw7/index.js
+++ homework/hw7/index.js
@@ -1,14 +1,42 @@
 $(document).ready(function(){
-    var title_url = "//cis.gvsu.edu/~scrippsj/cs371/hw/hw07/getTitles.php";
-    var movie_url = "//cis.gvsu.edu/~scrippsj/cs371/hw/hw07/getMovie.php";
+    var titleUrl = "//cis.gvsu.edu/~scrippsj/cs371/hw/hw07/getTitles.php";
+    var movieUrl = "//cis.gvsu.edu/~scrippsj/cs371/hw/hw07/getMovie.php";
     var titles = [];
-    var description = ""
-    $.getJSON(title_url, function(data) {
+    $.getJSON(titleUrl, function(data) {
         titles = data;
-        console.log(titles);
+        for (title in titles) {
+            $("#title-select").append("<option>" +
+                titles[title] + "</option>");
+        }
+        $.get((movieUrl + "?id=1"), function(data) {
+            updateMovieDescription(data);
+        
+        });
     });
-    $.get((movie_url + "?id=1"), function(data) {
-        description = data;
-        console.log(description);
+    $('#title-select').on('change', function() {
+        var option = $("#title-select").prop("selectedIndex");
+        $.get((movieUrl + "?id=" + (option + 1)), function(data) {
+            updateMovieDescription(data);
+        });
     });
+
+    function updateMovieDescription(description) {
+        var plot = "<h3>Plot</h3>";
+        var cast = "<h3>Cast</h3>";
+        
+        var tableIndex = description.indexOf("<table");
+        plot += "<p>" + description.substring(0, tableIndex) + "</p>";
+        cast += description.substring(tableIndex, tableIndex + "<table".length);
+        cast += " class=\"table table-hover\"";
+        var borderTag = "border=\"1\"";
+        var borderIndex = description.indexOf(borderTag); 
+        var borderEndIndex = borderTag.length + borderIndex;
+        cast += description.substring(borderEndIndex, description.length);
+                
+        $("#plot").empty();
+        $("#cast").empty();
+        
+        $("#plot").append(plot);
+        $("#cast").append(cast); 
+    }
 })
