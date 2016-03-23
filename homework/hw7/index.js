22bbe4aa14a947e97a63ad619fbad2ccd4f374c2
Josiah Campbell <campbjos@mail.gvsu.edu>
2016-03-22 21:51:05 -0400

    Add support for getmovie ajax call

--- homework/hw7/index.js
+++ homework/hw7/index.js
@@ -2,8 +2,13 @@ $(document).ready(function(){
     var title_url = "//cis.gvsu.edu/~scrippsj/cs371/hw/hw07/getTitles.php";
     var movie_url = "//cis.gvsu.edu/~scrippsj/cs371/hw/hw07/getMovie.php";
     var titles = [];
+    var description = ""
     $.getJSON(title_url, function(data) {
         titles = data;
         console.log(titles);
+    });
+    $.get((movie_url + "?id=1"), function(data) {
+        description = data;
+        console.log(description);
     });
 })
